3bbfbbaa73547672e486ecaf9c3064aebf26848b
dejiade-coder <dejiadegbite939@gmail.com>
2026-06-21 01:19:36 -1100

    Wire tenant provisioning password service

--- backend/src/modules/tenants/tenants.module.ts
+++ backend/src/modules/tenants/tenants.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { TenantsController } from './tenants.controller';
 import { TenantsService } from './tenants.service';
 
-@Module({ controllers: [TenantsController], providers: [TenantsService] })
+@Module({ imports: [AuthModule], controllers: [TenantsController], providers: [TenantsService] })
 export class TenantsModule {}
